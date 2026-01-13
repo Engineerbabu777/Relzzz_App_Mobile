625cba0afa3c59742ac13e6a746aa6089c4ccf2c
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-13 13:14:28 +0500

    add(file): a new empty file has been added

--- App.tsx
+++ App.tsx
@@ -4,6 +4,13 @@
 
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import {GoogleSignin} from '@react-native-google-signin/google-signin'
+
+GoogleSignin.configure({
+  webClientId:'1076122205215-csqq0gkfg8k9j4hs2upp4cg1oeivbgqg.apps.googleusercontent.com',
+  forceCodeForRefreshToken:true,
+  offlineAccess:false
+})
 
 const App = () => {
   return (
