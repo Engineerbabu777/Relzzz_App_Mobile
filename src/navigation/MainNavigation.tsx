41b048f80e10d4e3ac2e200f90d3fce4c15ce0a0
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-13 13:23:00 +0500

    add(MainNavigation): this new empty component has bene added

---
+++ src/navigation/MainNavigation.tsx
@@ -0,0 +1,17 @@
+
+
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MainNavigation = () => {
+  return (
+    <View>
+      <Text>MainNavigation</Text>
+    </View>
+  )
+}
+
+export default MainNavigation
+
+const styles = StyleSheet.create({})
