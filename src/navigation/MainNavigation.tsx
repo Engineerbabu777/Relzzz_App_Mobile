7ffa65b816109d849adc5b943c619b12cd17ef93
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-13 13:33:18 +0500

    add(MainNavigation): this is done

--- src/navigation/MainNavigation.tsx
+++ src/navigation/MainNavigation.tsx
@@ -1,7 +1,6 @@
 
 
 
-import { StyleSheet } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react'
 import { mergedStack } from './ScreenCollection';
