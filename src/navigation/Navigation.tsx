ec3873101b4efb820a8297082ee58ee8ed5e65d5
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-13 13:15:55 +0500

    add(Navigation): this has eben added

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Navigation: React.FC = () => {
+    return <NavigationContainer>
+        <MainNavigation />
+    </NavigationContainer>
+}
+
+export default Navigation;
