33e64492759a081a033b63e4adcf69d7ead0a205
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-13 14:03:09 +0500

    add(normalizeWidth): this has been added new method

--- src/utils/Scaling.ts
+++ src/utils/Scaling.ts
@@ -9,9 +9,9 @@ export const normalizeModerately = (size: number, factor = 0.5): number => {
   return PixelRatio.roundToNearestPixel(moderateScale(size, factor));
 };
 
-// export const normalizeWidth = (size: number): number => {
-//   return PixelRatio.roundToNearestPixel(scale(size));
-// };
+export const normalizeWidth = (size: number): number => {
+  return PixelRatio.roundToNearestPixel(scale(size));
+};
 
 // export const normalizeHeight = (size: number): number => {
 //   return PixelRatio.roundToNearestPixel(verticalScale(size));
