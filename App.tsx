bb40284980fe76addcacb74bb15492f489b5be4b
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-22 17:47:08 +0500

    add(App): this has been updated a bit

--- App.tsx
+++ App.tsx
@@ -1,25 +1,22 @@
-
-
-
-
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import {GoogleSignin} from '@react-native-google-signin/google-signin'
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import Navigation from './src/navigation/Navigation';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 GoogleSignin.configure({
-  webClientId:'1076122205215-csqq0gkfg8k9j4hs2upp4cg1oeivbgqg.apps.googleusercontent.com',
-  forceCodeForRefreshToken:true,
-  offlineAccess:false
-})
+  webClientId:
+    '1076122205215-csqq0gkfg8k9j4hs2upp4cg1oeivbgqg.apps.googleusercontent.com',
+  forceCodeForRefreshToken: true,
+  offlineAccess: false,
+});
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Navigation />
+    </GestureHandlerRootView>
+  );
+};
 
-export default App
-
-const styles = StyleSheet.create({})
+export default App;
