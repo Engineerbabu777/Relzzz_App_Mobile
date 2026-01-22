23f458f3b9affd2445930ad72869e17551745662
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-22 17:30:57 +0500

    add(dashboardStack): this has been updated

--- src/navigation/ScreenCollection.tsx
+++ src/navigation/ScreenCollection.tsx
@@ -1,34 +1,35 @@
-import LoginScreen from "../screens/auth/LoginScreen";
-import RegisterScreen from "../screens/auth/RegisterScreen";
-import SplashScreen from "../screens/auth/SplashScreen";
-import BottomTab from "./BottomTab";
-
+import LoginScreen from '../screens/auth/LoginScreen';
+import RegisterScreen from '../screens/auth/RegisterScreen';
+import SplashScreen from '../screens/auth/SplashScreen';
+import PickReelScreen from '../screens/reel/PickReelScreen';
+import BottomTab from './BottomTab';
 
 export const authStack = [
-    {
-        name: "LoginScreen",
-        component: LoginScreen
-    },
+  {
+    name: 'LoginScreen',
+    component: LoginScreen,
+  },
 
-    {
-        name: "SplashScreen",
-        component: SplashScreen
-    },
+  {
+    name: 'SplashScreen',
+    component: SplashScreen,
+  },
 
-    {
-        name: "RegisterScreen",
-        component: RegisterScreen
-    }
-]
+  {
+    name: 'RegisterScreen',
+    component: RegisterScreen,
+  },
+];
 
 export const dashboardStack = [
-    {
-        name: "BottomTab",
-        component: BottomTab
-    },
-   
-]
+  {
+    name: 'BottomTab',
+    component: BottomTab,
+  },
+  {
+    name: 'PickReelScreen',
+    component: PickReelScreen,
+  },
+];
 
-
-
-export const mergedStack = [...authStack,...dashboardStack];
+export const mergedStack = [...authStack, ...dashboardStack];
