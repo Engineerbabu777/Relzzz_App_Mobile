3b07f59e8537cd2bdb27d3731b194586079b0cd2
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-22 17:27:27 +0500

    add(PickReelScreen): this has been updated

---
+++ src/screens/reel/PickReelScreen.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+import React from 'react';
+import CustomView from '../../components/global/CustomView';
+
+export default function PickReelScreen() {
+  return <CustomView />;
+}
+
+const styles = StyleSheet.create({});
